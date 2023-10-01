341ba42d54eb905a57394bad8c08c3f382038440
zinakalu <zina.kalu@gmail.com>
2023-10-01 11:31:37 -0400

    css and arrow function

--- src/SortInput.js
+++ src/SortInput.js
@@ -1,42 +1,53 @@
-import React, { useState } from 'react'
-//this component collects input from user 
-function SortInput({onSort}) {
-    const [numberList, setNumberList] = useState([])
-    const [algorithm, setAlgorithm] = useState("bubble_sort")
+import React, { useState } from "react";
 
-    function onSortclicked(){
-        // /sorting/{sortAlgorithm}
-        console.log(numberList)
-        console.log(algorithm)
-        onSort(numberList, algorithm)
-    }
+function SortInput({ onSort }) {
+  const [numberList, setNumberList] = useState([]);
+  const [algorithm, setAlgorithm] = useState("bubble_sort");
 
-    function onNumbersChanged(e){
-        const value = e.target.value // "1,2,3,4,5" this is a string what we want is ["1", "2", "3", "4", "5"] => [1,2,3,4,5]. this is the next line
-        const newNumberList = value.split(",").map(val => parseInt(val)) //the map function loops through each element and converts them to integers
-        setNumberList(newNumberList) //updates the state to [1,2,3,4,5]
-    }
+  function onSortclicked() {
+    console.log(numberList);
+    console.log(algorithm);
+    onSort(numberList, algorithm);
+  }
 
-    function onAlgorithmChanged(e){
-        setAlgorithm(e.target.value)
-    }
+  function onNumbersChanged(e) {
+    const value = e.target.value;
+    const newNumberList = value.split(",").map((val) => parseInt(val));
+    setNumberList(newNumberList);
+  }
 
+  function onAlgorithmChanged(e) {
+    setAlgorithm(e.target.value);
+  }
 
+  function scrollToBottom() {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  }
 
   return (
     <>
-    <input id="unsortedNumbers" type="text" placeholder='Enter list of numbers' onChange={onNumbersChanged} ></input> 
-    <select id="sortAlgorithm" onChange={onAlgorithmChanged}>
+      <h3>Choose from a range of sorting algorithms!</h3>
+      <input
+        id="unsortedNumbers"
+        type="text"
+        placeholder="Enter list of numbers"
+        onChange={onNumbersChanged}
+      ></input>
+      <select id="sortAlgorithm" onChange={onAlgorithmChanged}>
         <option value="bubble_sort">Bubble sort</option>
         <option value="merge_sort">Merge sort</option>
         <option value="quick_sort">Quick sort</option>
         <option value="insertion_sort">Insertion sort</option>
         <option value="selection_sort">Selection sort</option>
-        
-    </select>
-    <button onClick={onSortclicked}>Sort!</button>
+      </select>
+      <button onClick={onSortclicked}>Sort!</button>
+
+      <div className="scrollDownArrow" onClick={scrollToBottom}></div>
     </>
-  )
+  );
 }
 
-export default SortInput
+export default SortInput;
--- src/SortedNumbers.js
+++ src/SortedNumbers.js
@@ -1,41 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import { motion } from 'framer-motion';
-import './SortedNumbers.css'
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./SortedNumbers.css";
 
-function SortedNumbers({ unsorted, sorted }) { //we're animating from unsorted to sorted
+function SortedNumbers({ unsorted, sorted }) {
+  const [displayedNumbers, setDisplayedNumbers] = useState(unsorted);
 
+  const largest = displayedNumbers.reduce(
+    (accumulatedValue, nextValue) =>
+      nextValue > accumulatedValue ? nextValue : accumulatedValue,
+    0
+  );
 
-    const [displayedNumbers, setDisplayedNumbers] = useState(unsorted)
+  function getHeightPercentage(value, largest) {
+    return (value / largest) * 100;
+  }
 
-    const largest = displayedNumbers.reduce((accumulatedValue, nextValue) => nextValue > accumulatedValue ? nextValue : accumulatedValue, 0)
-    //[1, 2, 3, 4, 5] => 15 reduce () applies a accumulative value to all elements in the array, and returns the final result.
-
-    function getHeightPercentage(value, largest){
-        return value / largest * 100;
+  useEffect(() => {
+    if (sorted) {
+      const handle = setTimeout(() => setDisplayedNumbers(sorted), 1000);
+      return () => clearTimeout(handle);
     }
-
-
-    useEffect(()=>{
-        if (sorted){
-
-        
-        const handle = setTimeout(()=> setDisplayedNumbers(sorted), 2000); //after 2 seconds the sorted numbers will be displayed
-        return ()=> clearTimeout(handle) //clears the timeout
-        }
-    }, [sorted])
-
-
+  }, [sorted]);
 
   return (
     <div className="sortOutputContainer">
-        {/* {sortedNumbers.join(",")} */}
-        {
-        displayedNumbers.map(number => <motion.div layout className="bar" key={number} style={{height: getHeightPercentage(number, largest) + "%"}}>
-        </motion.div>)} 
+      <AnimatePresence>
+        {displayedNumbers.map((number) => (
+          <motion.div
+            layout
+            initial={{ opacity: 0, height: "0%" }} // Initial state
+            animate={{
+              opacity: 1,
+              height: getHeightPercentage(number, largest) + "%",
+            }}
+            exit={{ opacity: 0, height: "0%" }}
+            className="bar"
+            key={number}
+          ></motion.div>
+        ))}
+      </AnimatePresence>
     </div>
-  )
+  );
 }
 
-export default SortedNumbers
-
-//wraps our divs with everything the library needs to animate
+export default SortedNumbers;
